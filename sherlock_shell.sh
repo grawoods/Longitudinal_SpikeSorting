a6abb04ac3e5d937602d1847449ea2e3914ea7ec
GraceWoods <GraceWoods@DN0a237835.SUNet>
2023-06-01 19:09:56 -0700

    Sherlock Testing

--- sherlock_shell.sh
+++ sherlock_shell.sh
@@ -1,7 +1,8 @@
 ml python/3.9.0
-ml numpy/1.20.3_py39
+ml py-numpy/1.20.3_py39
 ml py-scikit-learn/1.0.2_py39
 ml py-scipy/1.10.1_py39
+ml viz
 ml py-matplotlib/3.4.2_py39
 
 python3 sherlock_executable.py --datadir="$SCRATCH"/SHERLOCK_TEST/200516_M200212/
